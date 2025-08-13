9ddcc905eeb260af37e8f8e901a34febaf6862d8
Jackson Coxson <jkcoxson@gmail.com>
2025-08-13 17:24:59 -0600

    Implement clone for timesync

--- src/timesync.rs
+++ src/timesync.rs
@@ -13,7 +13,7 @@ use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::mem::size_of;
 
-#[derive(Debug, Serialize, Deserialize, Default)]
+#[derive(Clone, Debug, Serialize, Deserialize, Default)]
 pub struct TimesyncBoot {
     pub signature: u16,
     pub header_size: u16,
@@ -27,7 +27,7 @@ pub struct TimesyncBoot {
     pub timesync: Vec<Timesync>,
 }
 
-#[derive(Debug, Serialize, Deserialize, Default)]
+#[derive(Clone, Debug, Serialize, Deserialize, Default)]
 pub struct Timesync {
     // Timestamps are in UTC
     pub signature: u32,
